df598c43dea87f18bef0d5c88872dda1654bfa31
Leeseein <huangha607@naver.com>
2025-01-25 19:54:06 +0900

    Feat: view 컨트롤러 추가 - 서버사이드 컨트롤러

---
+++ src/main/java/com/hrm/controller/EmployeeViewController.java
@@ -0,0 +1,19 @@
+package com.hrm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeViewController {
+
+	/*
+	 * GET /employee
+	 * employee/index.html 페이지만 렌더링
+	 */
+	@GetMapping("")
+	public String getEmployeePage() {
+		return "employee/index";
+	}
+}

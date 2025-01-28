e92b318c1084d33c698c28a5125ee1b2b53bb03d
Leeseein <huangha607@naver.com>
2025-01-28 21:16:52 +0900

    Feat: 사원 추가 Service

--- src/main/java/com/hrm/service/EmployeeService.java
+++ src/main/java/com/hrm/service/EmployeeService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.hrm.dao.EmployeeDao;
+import com.hrm.dto.DepartmentDto;
 import com.hrm.dto.EmployeeDto;
 
 import lombok.RequiredArgsConstructor;
@@ -19,9 +20,26 @@ public class EmployeeService {
 	public List<EmployeeDto> employeesList() {
 		return employeeDao.employeesList();
 	}
- 
+
 	// 사원 상세정보 (사원번호, 생년월일, 직급 등...)
 	public EmployeeDto employeesDetail(int EmployeeID) {
 		return employeeDao.employeesDetail(EmployeeID);
+	}
+
+	// 부서 리스트
+	public List<DepartmentDto> departmentsList() {
+		return employeeDao.departmentList();
+	}
+
+	// 사원 추가
+	public EmployeeDto addEmployee(EmployeeDto employeeDto) {
+		// 사원을 추가하고
+		int result = employeeDao.addEmployee(employeeDto);
+
+		if (result > 0) {
+			// 추가된 사원의 정보를 반환
+			return employeeDao.employeesDetail(employeeDto.getEmployeeId());
+		}
+		return null;
 	}
 }

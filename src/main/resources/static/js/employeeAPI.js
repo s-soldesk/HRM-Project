8e30cc8284ed2ec181ebf10c1d42a385b2c5ae83
Leeseein <huangha607@naver.com>
2025-01-30 18:48:48 +0900

    Feat: 사원 수정 API 추가

--- src/main/resources/static/js/employeeAPI.js
+++ src/main/resources/static/js/employeeAPI.js
@@ -1,6 +1,6 @@
 const api = {
 
-	// 사원 관련 API
+	// 사원 CRUD API
 	async getEmployees() {
 		const response = await fetch('/employees/list');
 		return response.json();
@@ -14,6 +14,14 @@ const api = {
 	async addEmployee(data) {
 		const response = await fetch('/employees/add', {
 			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(data)
+		});
+		return response.json();
+	},
+	async updateEmployee(id, data) {
+		const response = await fetch(`/employees/${id}`, {
+			method: 'PUT',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify(data)
 		});

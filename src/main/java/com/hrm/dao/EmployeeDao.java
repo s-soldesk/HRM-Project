b8a55cac865198a8d5edf1c5da37bdd1b678dc05
Leeseein <huangha607@naver.com>
2025-01-30 18:49:10 +0900

    Feat: 사원 수정 DAO 추가

--- src/main/java/com/hrm/dao/EmployeeDao.java
+++ src/main/java/com/hrm/dao/EmployeeDao.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.hrm.dto.DepartmentDto;
 import com.hrm.dto.EmployeeDto;
@@ -15,7 +16,9 @@ import com.hrm.dto.EmployeeDto;
 @Mapper
 public interface EmployeeDao {
 	/*
-	 * 사원 + 부서 조인 하여 전체사원을 조회 (리스트) Employee 테이블과 Department 테이블을 NATURAL JOIN하여
+	 * 사원 + 부서 조인 하여 전체사원을 조회
+	 * GET /employees/list
+	 *  (리스트) Employee 테이블과 Department 테이블을 NATURAL JOIN하여
 	 * 사원번호 + 사원 이름 + 부서 이름만 가져옴.
 	 */
 	@Select({ """
@@ -29,7 +32,10 @@ public interface EmployeeDao {
 	// "DepartmentName" 컬럼의 값을 EmployeeDto 의 Department 객체의 departmentname 필드에 넣음.
 	List<EmployeeDto> employeesList();
 
-	// 사원 세부정보 조회
+	/*
+	 *  사원 세부정보 조회
+	 *  GET /employees/{employeeID}
+	 */
 	@Select({ """
 				SELECT
 					e.EmployeeID,
@@ -58,6 +64,7 @@ public interface EmployeeDao {
 
 	/*
 	 * 부서 리스트 조회
+	 * GET /employees/department/list
 	 * 사원을 추가하는 모달에서 사용
 	 */
 	@Select({
@@ -67,6 +74,7 @@ public interface EmployeeDao {
 	
 	/*
 	 * 사원 추가
+	 * POST /employees/add
 	 */
 	@Insert({ """
 			INSERT INTO Employee(
@@ -82,7 +90,7 @@ public interface EmployeeDao {
 			)
 			VALUES(
 				#{name},
-	            #{dateBirth},
+	            #{dateOfBirth},
 	            #{gender},
 	            #{departmentId},
 	            #{position},
@@ -96,4 +104,26 @@ public interface EmployeeDao {
 	// INSERT 후에 생성된 EmployeeID 값을 자동으로 EmployeeDto 객체의 employeeId 필드에 설정
 	@Options(useGeneratedKeys = true, keyProperty =  "employeeId")
 	int addEmployee(EmployeeDto employeeDto);
+	
+	/*
+	 *  사원 수정
+	 *  PUT /employees/{id}
+	 */
+	@Update({"""
+        UPDATE Employee 
+        SET
+			Name = #{name},
+	        DateOfBirth = #{dateOfBirth},
+	        Gender = #{gender},
+	        DepartmentID = #{departmentId},
+	        Position = #{position},
+	        HireDate = #{hiredate},
+	        Status = #{status},
+	        PhoneNumber = #{phonenumber},
+	        Email = #{email}
+        WHERE EmployeeID = #{employeeId}
+        """
+	})
+	int updateEmployee(EmployeeDto employeeDto);
+	
 }

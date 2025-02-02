eb7ff106dbe41049a6b63ed112235bf65fe28719
fodyd132 <dydfo66@gmail.com>
2025-02-03 01:52:57 +0900

    Feat: 로그인, 로그아웃, 메인화면 구현

--- src/main/java/com/hrm/HrmProjectApplication.java
+++ src/main/java/com/hrm/HrmProjectApplication.java
@@ -1,11 +1,12 @@
 package com.hrm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hrm.dao")
 public class HrmProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HrmProjectApplication.class, args);
 	}

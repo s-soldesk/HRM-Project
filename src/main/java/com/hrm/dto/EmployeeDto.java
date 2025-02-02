a843c3f490e6d99a6664b10fdc1b0a7f2c90e0a9
Limjieung <oneji324@naver.com>
2025-02-02 11:27:44 +0900

    Dto: private String lastMessage; 추가

--- src/main/java/com/hrm/dto/EmployeeDto.java
+++ src/main/java/com/hrm/dto/EmployeeDto.java
@@ -16,7 +16,8 @@ public class EmployeeDto {
 	private String position; // 직급
 	private LocalDate hiredate; // 입사일
 	private String status; // 재직상태
-	private Integer phonenumber; // 핸드폰
+	private String phonenumber; // 핸드폰
 	private String email; // 이메일
 	private DepartmentDto department; // 부서정보 매핑
+	private String lastMessage; // 전 메세지 
 }

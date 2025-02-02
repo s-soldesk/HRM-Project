c153d3fec8664520f009c715b37c4037b4ec668b
Limjieung <oneji324@naver.com>
2025-02-02 15:05:18 +0900

    Repository 이미지 변경을 위한 dir 저장 매체 및 필요 mapper,service 생성

---
+++ src/main/java/com/hrm/mapper/UserAccountsMapper.java
@@ -0,0 +1,17 @@
+package com.hrm.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.hrm.dto.UserAccountDto;
+
+@Mapper
+public interface UserAccountsMapper {
+    UserAccountDto findByEmployeeIdAndPassword(@Param("employeeId") Integer employeeId, 
+                                             @Param("password") String password);
+    
+    UserAccountDto findByEmployeeId(Integer employeeId);
+    int updatePassword(@Param("employeeId") Integer employeeId, 
+                      @Param("newPassword") String newPassword);
+}

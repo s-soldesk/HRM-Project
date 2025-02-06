e254b0c9077f8d2bcaab277e314f1205ef93b602
Leeseein <huangha607@naver.com>
2025-02-06 17:02:16 +0900

    Feat: 채용게시판 JPA Repository 구현

---
+++ src/main/java/com/hrm/repository/RecruitmentPostRepository.java
@@ -0,0 +1,15 @@
+package com.hrm.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hrm.entity.RecruitmentPostEntity;
+import com.hrm.enums.PostStatus;
+
+@Repository
+public interface RecruitmentPostRepository extends JpaRepository<RecruitmentPostEntity, Long> {
+	// status 로 모든 채용공고를 찾는 메소드
+	List<RecruitmentPostEntity> findByStatus(PostStatus status);
+}

8fa400a914975f3c0157b35dfc4157f1715a665e
Leeseein <huangha607@naver.com>
2025-02-07 16:22:19 +0900

    Fix: Table 이름 명시적 지정

--- src/main/java/com/hrm/entity/RecruitmentPostEntity.java
+++ src/main/java/com/hrm/entity/RecruitmentPostEntity.java
@@ -14,12 +14,14 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity // 이 클래스는 JPA 엔터티. 데이터베이스 테이블과 매핑
 @Getter
 @Setter
+@Table(name = "RecruitmentPost")
 public class RecruitmentPostEntity {
 	@Id // 기본 키
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 생성 전략. IDENTITY는 자동 증가
@@ -30,7 +32,7 @@ public class RecruitmentPostEntity {
 	@Column(columnDefinition = "TEXT") // 텍스트타입
 	private String content;
 
-	@Enumerated(EnumType.STRING) // enum 값을 문자열로 저장 
+	@Enumerated(EnumType.STRING) // enum 값을 문자열로 저장
 	private PostStatus status;
 
 	private LocalDate startDate;

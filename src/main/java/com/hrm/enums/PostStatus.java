cd57376dbb0a093c5753c3be9438fb57fea3887d
Leeseein <huangha607@naver.com>
2025-02-07 16:21:21 +0900

    Fix: 게시글 상태 변수명 수정 - description -> status

--- src/main/java/com/hrm/enums/PostStatus.java
+++ src/main/java/com/hrm/enums/PostStatus.java
@@ -3,14 +3,14 @@ package com.hrm.enums;
 public enum PostStatus {
 	ACTIVE("진행중"), CLOSED("마감됨");
 
-	private final String description; // "진행중", "마감됨" 한글을 저장하기 위한 문자열
+	private final String status; // "진행중", "마감됨" 한글을 저장하기 위한 문자열
 
-	private PostStatus(String description) {
-		this.description = description;
+	private PostStatus(String status) {
+		this.status = status;
 	}
 
 	public String getStatus() {
-		return description;
+		return status;
 	}
 
 }

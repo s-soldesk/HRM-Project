c3435be5ca8ca1a77b216e660b3db53d70020ff6
Leeseein <huangha607@naver.com>
2025-02-12 15:23:31 +0900

    Fix: UserAccount테이블 아이디타입 수정 - 사원ID를 이메일로 하기 때문에 integer->string

--- src/main/java/com/hrm/dto/UserAccountDto.java
+++ src/main/java/com/hrm/dto/UserAccountDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 public class UserAccountDto {
 
 	private Integer userId; // 사용자 id
-	private Integer employeeId; // 사원 id
+	private String employeeId; // 사원 id (이메일로 저장되기때문에 integer -> string)
 	private String username; // 사용자 이름
 	private String password; // 사용자 비밀번호, Spring security로 암호화 예정
 	private Role role; // 권한(관리자 or 인사팀)

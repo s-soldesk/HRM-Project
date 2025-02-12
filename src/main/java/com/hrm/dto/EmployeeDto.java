2d0faf2874029b56b0a871078ef16847331b91d6
Leeseein <huangha607@naver.com>
2025-02-12 12:28:07 +0900

    Refactor: 사원 직급 타입변경 - String -> Position(ENUM)

--- src/main/java/com/hrm/dto/EmployeeDto.java
+++ src/main/java/com/hrm/dto/EmployeeDto.java
@@ -2,6 +2,8 @@ package com.hrm.dto;
 
 import java.time.LocalDate;
 
+import com.hrm.enums.Position;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,14 +15,14 @@ public class EmployeeDto {
 	private LocalDate dateOfBirth; // 생일
 	private String gender; // 성별
 	private Integer departmentId; // 부서번호
-	private String position; // 직급
+	private Position position; // 직급
 	private LocalDate hiredate; // 입사일
 	private String status; // 재직상태(Active 재직, Inactive 휴직, Retirement 퇴사)
 	private String phonenumber; // 핸드폰
 	private String email; // 이메일
-	
+
 	// ------------------------- 추가
-	
+
 	private String departmentName; // 부서 이름
 	private DepartmentDto department; // 부서정보 매핑
 }

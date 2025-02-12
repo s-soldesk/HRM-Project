a41f2585bbfdad90e25c30699e137ff3621fe597
shinhan20180741 <ktm005005@gmail.com>
2025-02-12 12:21:48 +0900

    FIx - develop 브랜치와 merge 수정

--- src/main/java/com/hrm/dto/EmployeeDto.java
+++ src/main/java/com/hrm/dto/EmployeeDto.java
@@ -1,21 +1,24 @@
 package com.hrm.dto;
 
 import java.time.LocalDate;
-
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class EmployeeDto {
 	private Integer employeeId; // 사원번호
 	private String name; // 사원이름
-	private LocalDate dateBirth; // 생일
+	private LocalDate dateOfBirth; // 생일
 	private String gender; // 성별
 	private Integer departmentId; // 부서번호
 	private String position; // 직급
 	private LocalDate hiredate; // 입사일
-	private String status; // 재직상태
+	private String status; // 재직상태(Active 재직, Inactive 휴직, Retirement 퇴사)
 	private String phonenumber; // 핸드폰
 	private String email; // 이메일
+
+	// 추가된 필드 유지
+	private String departmentName; // 부서 이름
 	private DepartmentDto department; // 부서정보 매핑
-	private Integer roleId; // 접근 권한
 }

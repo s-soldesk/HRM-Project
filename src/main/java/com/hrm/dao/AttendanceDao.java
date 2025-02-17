ac8f643dae9ee05a5ce1a3e27fd569b33c3eab5b
shinhan20180741 <ktm005005@gmail.com>
2025-02-17 09:45:13 +0900

    FIx - 로그인한 사용자의 사원 ID 불러오도록 수정

--- src/main/java/com/hrm/dao/AttendanceDao.java
+++ src/main/java/com/hrm/dao/AttendanceDao.java
@@ -6,37 +6,33 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Mapper
 public interface AttendanceDao {
 
 	// 근태 기록 조회
-	 @Select("""
-	            SELECT a.AttendanceID, a.EmployeeID, e.Name AS employeeName, a.CheckInTime, a.CheckOutTime, a.AttendanceType, a.Remarks, a.Date
-	            FROM Attendance a
-	            JOIN Employee e ON a.EmployeeID = e.EmployeeID
-	            WHERE (#{employeeId} IS NULL OR a.EmployeeID = #{employeeId})
-	              AND (#{name} IS NULL OR e.Name LIKE CONCAT('%', #{name}, '%'))
-	              AND (#{startDate} IS NULL OR a.Date >= #{startDate})
-	              AND (#{endDate} IS NULL OR a.Date <= #{endDate})
-	              AND (#{attendanceType} IS NULL OR a.AttendanceType = #{attendanceType})
-	            UNION ALL
-	            SELECT NULL AS AttendanceID, s.EmployeeID, e.Name AS employeeName, NULL AS CheckInTime, NULL AS CheckOutTime, 'Leave' AS AttendanceType, s.Reason AS Remarks, s.StartDate AS Date
-	            FROM Schedule s
-	            JOIN Employee e ON s.EmployeeID = e.EmployeeID
-	            WHERE (#{employeeId} IS NULL OR s.EmployeeID = #{employeeId})
-	              AND (#{name} IS NULL OR e.Name LIKE CONCAT('%', #{name}, '%'))
-	              AND (#{startDate} IS NULL OR s.StartDate >= #{startDate})
-	              AND (#{endDate} IS NULL OR s.EndDate <= #{endDate})
-	        """)
-	    List<AttendanceDto> searchAttendanceRecords(
-	        @Param("employeeId") String employeeId,
-	        @Param("name") String name,
-	        @Param("startDate") String startDate,
-	        @Param("endDate") String endDate,
-	        @Param("attendanceType") String attendanceType
-	    );
+	@Select("""
+		    SELECT a.AttendanceID, a.EmployeeID, e.Name AS employeeName, 
+		           a.CheckInTime, a.CheckOutTime, a.Status, a.Date
+		    FROM Attendance a
+		    JOIN Employee e ON a.EmployeeID = e.EmployeeID
+		    WHERE (#{employeeId} IS NULL OR a.EmployeeID = #{employeeId})
+		    AND (#{name} IS NULL OR e.Name LIKE CONCAT('%', #{name}, '%'))
+		    AND (#{startDate} IS NULL OR a.Date >= #{startDate})
+		    AND (#{endDate} IS NULL OR a.Date <= #{endDate})
+		    AND (#{status} IS NULL OR a.Status = #{status})
+		""")
+		List<AttendanceDto> searchAttendanceRecords(
+		    @Param("employeeId") String employeeId,
+		    @Param("name") String name,
+		    @Param("startDate") String startDate,
+		    @Param("endDate") String endDate,
+		    @Param("status") String status
+		);
+ 
 
 	
 	// 특정한 근태 기록과 해당 사원 이름 조회
@@ -47,7 +43,21 @@ public interface AttendanceDao {
 	
     
 	// 근태 기록 수정
-	@Update("UPDATE Attendance SET CheckInTime = #{checkInTime}, CheckOutTime = #{checkOutTime}, AttendanceType = #{attendanceType}, Remarks = #{remarks} WHERE AttendanceID = #{attendanceId}")
-    void updateAttendance(AttendanceDto attendance);
+	@Update("UPDATE Attendance " +
+	        "SET CheckInTime = CASE WHEN #{status} != 'Leave' THEN #{checkInTime} ELSE NULL END, " +
+	        "CheckOutTime = CASE WHEN #{status} != 'Leave' THEN #{checkOutTime} ELSE NULL END, " +
+	        "HoursWorked = CASE WHEN #{status} != 'Leave' THEN #{hoursWorked} ELSE NULL END, " +
+	        "OvertimeHours = CASE WHEN #{status} != 'Leave' THEN #{overtimeHours} ELSE NULL END, " +
+	        "Status = #{status} " +  // status 컬럼 업데이트
+	        "WHERE EmployeeID = #{employeeId} AND Date = #{date}")
+	void updateAttendance(@Param("employeeId") int employeeId,
+	                      @Param("date") LocalDate date,
+	                      @Param("checkInTime") LocalTime checkInTime,
+	                      @Param("checkOutTime") LocalTime checkOutTime,
+	                      @Param("hoursWorked") Double hoursWorked,
+	                      @Param("overtimeHours") Double overtimeHours,
+	                      @Param("status") String status);
+
+
 
 }

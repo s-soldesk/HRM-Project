8cf13d351e9840132874c3a7ef14604e02be9c8c
fodyd132 <dydfo66@gmail.com>
2025-02-26 11:30:11 +0900

    Fix: 로그인 수정중

--- src/main/java/com/hrm/controller/LoginController.java
+++ src/main/java/com/hrm/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.hrm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -21,4 +22,3 @@ public class LoginController {
         return "login";
     }
 }
-
